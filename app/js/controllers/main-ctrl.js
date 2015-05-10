f67e0dbc052471f9ae713416072e665755cbcd48
dbertella <daniele.bertella@gmail.com>
2015-05-10 13:39:38 +0200

    Added style to form and to result

--- app/js/controllers/main-ctrl.js
+++ app/js/controllers/main-ctrl.js
@@ -8,27 +8,27 @@ function mainCtrl($scope, $q, $state, $stateParams, $timeout, flightsApi) {
         $scope.startdate = '2015-5-10';
         $scope.enddate = '2015-5-20';
 
-//    $q.all([
-//        flightsApi.getAirports()
-////        flightsApi.getCountries(),
-////        flightsApi.getRoutes()
-//    ])
-//    .then(function (resultSet) {
-//       	$scope.airports = resultSet[0];
-////        $scope.countries = resultSet[1];
-////    	$scope.routes = resultSet[2];
-//       
-//        
-//        $scope.submitForm = function(isValid) {
-//
-//            // check to make sure the form is completely valid
-//            if (isValid) {
-//                
-//                flightsApi.getCheapFlights($scope.from, $scope.to, $scope.startdate, $scope.enddate).then(function (data) {
-//                    console.log(data);
-//                });
-//            }
-//
-//        };
-//    });
+    $q.all([
+        flightsApi.getAirports()
+//        flightsApi.getCountries(),
+//        flightsApi.getRoutes()
+    ])
+    .then(function (resultSet) {
+       	$scope.airports = resultSet[0];
+//        $scope.countries = resultSet[1];
+//    	$scope.routes = resultSet[2];
+       
+        
+        $scope.submitForm = function(isValid) {
+
+            // check to make sure the form is completely valid
+            if (isValid) {
+                
+                flightsApi.getCheapFlights($scope.from, $scope.to, $scope.startdate, $scope.enddate).then(function (data) {
+                    $scope.flights = data.flights;
+                });
+            }
+
+        };
+    });
 }
